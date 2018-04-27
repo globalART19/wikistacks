6de9ac1b10833a9dbde63458f1d4102460f6f94d
Dan Gutt <dgutt10@gmail.com>
2018-04-27 10:51:20 -0500

    Dan copy 2

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const router = require('./routes/router')
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(router);
+
+const Port = 1337;
+
+app.listen(Port, () => {
+    console.log(`app listen on ${Port}`);
+})
+
+
---
+++ routes/router.js
@@ -0,0 +1,17 @@
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
+const main = require('../views/main');
+
+
+router.get('/', (req, res, next) =>{
+    try{
+        res.send(main(''));
+    } catch(err){
+        next(err);
+    }
+});
+
+
+module.exports = router;
